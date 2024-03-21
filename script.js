8f61c899db182bc037da69e5113ac4e0b41d575f
Mooton PK <mootonpk@Mootons-MacBook-Air.local>
2024-03-21 13:28:26 +0700

    add all item

---
+++ script.js
@@ -0,0 +1,18 @@
+function openRulebook(){
+	window.open('./assets/path.pdf', '_blank');
+}
+
+function openVideo(){
+}
+
+function openKey1(){
+}
+
+function openKey2(){
+}
+
+function openKey3(){
+}
+
+function openKey4(){
+}
